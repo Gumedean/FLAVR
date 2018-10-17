ec09fc936a089bbd6ec95f56c670029c6f806000
Gumedean <gumedean@icloud.com>
2018-10-17 16:35:53 +0200

    Created file

---
+++ FLAVR-RECIPE/FLAVR-RECIPE/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FLAVR-RECIPE
+//
+//  Created by Andile Gumede on 2018/10/17.
+//  Copyright © 2018 Andile Gumede. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
